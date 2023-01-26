508704386ddcf2c91d19b2a1240d287f1e60d1af
stephanefly <faure_stephane@hotmail.fr>
2023-01-26 22:23:10 +0100

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,12 +15,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&8lr(48glyi0+^f9rqmo^74+nbtyr3k*c5px18e2mw_wtf5u-e'
+# Initialise environment variables
+# Initialise environment variables
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
